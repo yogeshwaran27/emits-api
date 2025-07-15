10d2da8f2f910038897ff754787c595952338aac
yogeshwaran27 <50258859+yogeshwaran27@users.noreply.github.com>
2025-07-16 00:15:16 +0530

    config change

--- src/config.ts
+++ src/config.ts
@@ -6,16 +6,12 @@ const configurations =  {
     DB_USER:process.env.DB_USER,
     DB_PASSWORD:process.env.DB_PASSWORD,
     FRONTEND_REDIRECT:process.env.FRONTEND_REDIRECT,
-    NODE_ENV:process.env.NODE_ENV,
+    NODE_ENV:process.env.ENV,
     DB_NAME:process.env.DB_NAME,
     DB_HOST:process.env.DB_HOST,
     DB_PORT:process.env.DB_PORT,
     JWT_CONSTANT:process.env.JWT_CONSTANT,
     FRONTEND_URL:process.env.FRONTEND_URL,
-    SMTP_HOST:process.env.SMTP_HOST,
-    SMTP_PORT:process.env.SMTP_PORT,
-    SMTP_PASSWORD:process.env.SMTP_PASSWORD,
-    SMTP_USER:process.env.SMTP_USER
   };
 
 export default configurations;
